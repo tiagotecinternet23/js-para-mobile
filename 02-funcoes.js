7a5c029229406f892a1bb30855c12bc09a292967
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-21 10:50:29 -0300

    Correção exercícios

--- 02-funcoes.js
+++ 02-funcoes.js
@@ -25,3 +25,29 @@ function exemplo2(){
 exemplo2();
 
 
+/* Exercícios
+1) Crie uma função (qualquer sintaxe) que receba dois valores numéricos, calcule a diferença entre
+eles e retorne o resultado.*/
+function exercicio(valor1, valor2){
+    /* Versão 1: variável de escopo LOCAL */
+    // let resultado = valor1 - valor2;
+    // return resultado;
+
+    /* Versão 2 */
+    return valor1 - valor2;
+}
+
+/*2) Chame esta função duas vezes passando valores
+diferentes e guardando os resultados em variáveis de escopo global.*/
+let resultado1 = exercicio(10, 5);
+let resultado2 = exercicio(120, 66);
+let resultado3 = exercicio(66, 120);
+
+/*3) Mostre os valores processados pela função no terminal. */
+console.log(resultado1);
+console.log(resultado2);
+
+// Garantindo que o resultado seja absoluto (positivo)
+console.log( Math.abs(resultado3) );
+
+
