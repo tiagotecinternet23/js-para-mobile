9b6acb27936ad406f9f2711e7d6870845f387ec1
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-22 11:12:40 -0300

    Import de funções via alias

--- 04-codigo-modular.js
+++ 04-codigo-modular.js
@@ -6,11 +6,15 @@ para evitar conflitos entre módulos de mesmo nome */
 import { pessoa, livro, alunos as reprovados } from "./modulos/dados.js";
 import alunos from "./modulos/alunos.js";
 
-import { 
-    converteMaiusculas, 
-    converteMinusculas,
-    formataMoeda
-} from "./modulos/funcoes.js";
+/* Importação manual de cada função */
+// import { 
+//     converteMaiusculas, 
+//     converteMinusculas,
+//     formataMoeda
+// } from "./modulos/funcoes.js";
+
+/* Importação das funções através de um apelido/alias */
+import * as minhasFuncoes from "./modulos/funcoes.js";
 
 
 console.log(reprovados); // usando o módulo alunos através de apelido
@@ -29,14 +33,14 @@ console.log(livro.titulo);
 console.log(livro.volume);
 
 
-console.log( converteMaiusculas(reprovados[0]) );
-console.log( converteMinusculas(alunos[0]) );
+console.log( minhasFuncoes.converteMaiusculas(reprovados[0]) );
+console.log( minhasFuncoes.converteMinusculas(alunos[0]) );
 
 let preco1 = 1200.88;
 let preco2 = 1000000.75;
 let preco3 = 500.8452;
 
-console.log( formataMoeda(preco1) );
-console.log( formataMoeda(preco2) );
-console.log( formataMoeda(preco3) );
+console.log( minhasFuncoes.formataMoeda(preco1) );
+console.log( minhasFuncoes.formataMoeda(preco2) );
+console.log( minhasFuncoes.formataMoeda(preco3) );
 
