151cc64509a60947b37e618b50f03c6e325140e2
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-22 11:52:51 -0300

    Update 05-operador-spread.js

--- 05-operador-spread.js
+++ 05-operador-spread.js
@@ -4,8 +4,25 @@
 Copiar elementos/dados de um array/objeto
 para dentro de outro array/objeto. */
 
+// Spread com arrays
 const bandas = ["Pink Floyd", "Rush", "Yes"];
 const maisBandas = [...bandas, "Slayer", "Nightwish"];
 
 console.log(bandas);
 console.log(maisBandas);
+
+// Spread com objetos
+const cliente = {
+    nome: "Seu Madruga",
+    cidade: "São Paulo",
+    idade: 55
+};
+
+const novosDados = {
+    ...cliente, // Copiando as props. do cliente pra cá
+    estado: "SP",
+    pedido: "123xyz",
+    total: 5000.88,
+    idade: 60 // sobreescrevendo valores de propriedades
+};
+console.log(novosDados);
