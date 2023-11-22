bb07199a6fcb578d84f3bda2553848b0d1ec9e79
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-22 11:43:59 -0300

    Operador Spread com Array

---
+++ 05-operador-spread.js
@@ -0,0 +1,11 @@
+// 05-operador-spread.js
+
+/* ...spread -> espalhar 
+Copiar elementos/dados de um array/objeto
+para dentro de outro array/objeto. */
+
+const bandas = ["Pink Floyd", "Rush", "Yes"];
+const maisBandas = [...bandas, "Slayer", "Nightwish"];
+
+console.log(bandas);
+console.log(maisBandas);
