a918d0bded6463520123d17be12714c3e1b41ccb
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-22 08:45:20 -0300

    Create 03-template-literal.js

---
+++ 03-template-literal.js
@@ -0,0 +1,17 @@
+// 03-template-literal.js
+
+const nome = "Chapolin";
+const sobrenome = "Colorado";
+let idade = 25;
+let cidade = "São Paulo";
+let estado = "SP";
+
+/* Concatenação tradicional usando operador + 
+Meu nome é Chapolin Colorado, tenho 25 anos e moro atualmente
+na cidade de São Paulo - SP. */
+let mensagem1 = "Meu nome é "+nome+" "+sobrenome+", tenho ";
+
+console.log(mensagem1);
+
+
+
