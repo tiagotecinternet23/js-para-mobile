9e436ce71361ec905395e720ae3dcf0553d1324e
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-23 10:57:12 -0300

    Map de titulos

--- 08-map.js
+++ 08-map.js
@@ -1,7 +1,7 @@
-// 08-map.js
+import cursos from "./modulos/cursos.js";
 
 /* map (mapear, mapeia - transformar em outra coisa) 
-Passa por elementos de um array e realiza operações em cada um deles através de uma função, gerando um novo array. */
+Passa por elementos de um array e realiza operações em cada um deles através de uma função (callback), gerando um novo array. */
 
 // Exemplo 1
 const numeros = [10, 20, 5, 12, 66, 50];
@@ -10,3 +10,13 @@ console.log(numeros);
 const numerosDobrados = numeros.map( numero => numero * 2 );
 console.log(numerosDobrados);
 
+console.log("-----------");
+
+console.log(cursos);
+
+// Exemplo 2: gerar um novo array apenas com os nomes dos cursos
+const titulos = cursos.map( (curso) => {
+    return curso.titulo
+} );
+
+console.log(titulos);
