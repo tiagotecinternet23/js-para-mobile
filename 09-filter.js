7484e33d56471cc00b254fd130458a21e33fa97e
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-24 08:36:56 -0300

    Create 09-filter.js

---
+++ 09-filter.js
@@ -0,0 +1,9 @@
+/* filter (filtrar) 
+Passa por elementos de um array (usando uma função callback) e retornar valores de acordo com uma ou mais condições gerando um novo array. */
+
+// Exemplo 1
+const vendas = [1500, 3000, 1000, 500, 750];
+const meta = 1000;
+
+// Gerando um array com os valores que bateram a meta
+
