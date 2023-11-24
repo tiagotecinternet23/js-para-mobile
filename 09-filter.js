63b51d788c991a590c2f3526ca017f67e9f463a9
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-11-24 08:55:23 -0300

    Update 09-filter.js

--- 09-filter.js
+++ 09-filter.js
@@ -2,8 +2,27 @@
 Passa por elementos de um array (usando uma função callback) e retornar valores de acordo com uma ou mais condições gerando um novo array. */
 
 // Exemplo 1
-const vendas = [1500, 3000, 1000, 500, 750];
+const vendas = [1500, 3000, 1000, 500, 750, 5000, 2000];
 const meta = 1000;
+console.log(vendas);
 
 // Gerando um array com os valores que bateram a meta
+const vendasAcimaDaMeta = vendas.filter( (venda) => {
+    return venda >= meta
+} );
 
+console.log(vendasAcimaDaMeta);
+
+// Exemplo 2
+const alunos = [
+    "Nicolas", "Eliel", "André", "Kaue", "Barbosa",
+    "Aline", "Melissa", "Marina", "Mônica"
+];
+
+console.log(alunos);
+
+const resultados = alunos.filter( aluno => {
+    return aluno.startsWith("M") || aluno.startsWith("m")
+} )
+
+console.log(resultados);
